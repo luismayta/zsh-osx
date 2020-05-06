b0968a31b34e8622c98770d2d8a04f6582102f8f
@slovacus <slovacus@gmail.com>
2020-05-06 00:29:47 -0500

    refactor: implement settings app store (#10)

--- zsh-osx.zsh
+++ zsh-osx.zsh
@@ -8,6 +8,7 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 osx_package_name=osx
+[ -z "${COMPUTER_NAME}" ] && export COMPUTER_NAME="Osiris"
 
 function osx::dependences {
     message_info "Installing dependences for ${osx_package_name}"
@@ -50,6 +51,49 @@ function osx::ux {
 
 }
 
+###############################################################################
+# 🍎 Mac App Store
+###############################################################################
+function osx::appStore {
+    defaults write com.apple.appstore ShowDebugMenu -bool true
+    defaults write com.apple.commerce AutoUpdate -bool true
+    defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+    defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+    defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+    # Check for software updates daily, not just once per week
+    defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+    # Allow the App Store to reboot machine on macOS updates
+    defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+}
+
+###############################################################################
+# ⌨️ Keyboard
+###############################################################################
+function osx::keyboard {
+    defaults write -g KeyRepeat -int 3
+    defaults write -g InitialKeyRepeat -int 15
+
+    # Disable press-and-hold for keys in favour of key repeat
+    defaults write -g ApplePressAndHoldEnabled -bool true
+
+    # Shortcut to maximize window
+    defaults write -g NSUserKeyEquivalents -dict-add "Zoom" -string "@~^f"
+
+    # Disable automatic modifications of entered text
+    defaults write -g NSAutomaticCapitalizationEnabled -bool false
+    defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+    defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+    defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+    defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+    # Enable full keyboard access for all controls
+    # (e.g. enable Tab in modal dialogs)
+    defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+}
+
 ################################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input
 ###############################################################################
@@ -77,14 +121,14 @@ function osx::trackpad {
 # Screen
 ###############################################################################
 function osx::screen {
-
+    local screenshot_location
     message_info "Requiring password immediately after sleep or screen saver begins"
     defaults write com.apple.screensaver askForPassword -int 1
     defaults write com.apple.screensaver askForPasswordDelay -int 0
 
     message_info "Setting Screenshot location to ~/Desktop/Screenshots"
-    mkdir -p ~/Desktop/Screenshots
     screenshot_location="${HOME}/Desktop/Screenshots"
+    mkdir -p "${screenshot_location}"
     defaults write com.apple.screencapture location -string "${screenshot_location}"
 
     message_info "Setting screenshot format to PNG"
@@ -123,20 +167,107 @@ function osx::finder {
     message_info "Allowing text selection in Quick Look/Preview in Finder by default"
     defaults write com.apple.finder QLEnableTextSelection -bool true
 
-    message_info "Enable snap-to-grid for icons on the desktop and in other icon views"
+    defaults write com.apple.finder DisableAllAnimations -bool true
+    defaults write -g AppleShowAllExtensions -bool true
+    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+    defaults write com.apple.finder NewWindowTarget -string "PfDe"
+    defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+    defaults write com.apple.finder ShowPathbar -bool true
+    defaults write com.apple.finder ShowStatusBar -bool true
+    defaults write com.apple.finder FinderSpawnTab -bool false
+    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+    defaults write com.apple.finder _FXSortFoldersFirst -bool true
+    defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+    # Toolbar icons
+    defaults write com.apple.finder 'NSToolbar Configuration Browser' '{
+    "TB Default Item Identifiers" =     (
+        "com.apple.finder.BACK",
+        NSToolbarFlexibleSpaceItem,
+        "com.apple.finder.SWCH",
+        "com.apple.finder.ARNG",
+        "com.apple.finder.ACTN",
+        "com.apple.finder.SHAR",
+        "com.apple.finder.LABL",
+        NSToolbarFlexibleSpaceItem,
+        NSToolbarFlexibleSpaceItem,
+        "com.apple.finder.SRCH"
+    );
+    "TB Display Mode" = 2;
+    "TB Icon Size Mode" = 1;
+    "TB Is Shown" = 1;
+    "TB Item Identifiers" =     (
+        "com.apple.finder.BACK",
+        NSToolbarFlexibleSpaceItem,
+        "com.apple.finder.SWCH",
+        "com.apple.finder.ARNG",
+        NSToolbarSpaceItem,
+        "com.apple.finder.NFLD",
+        NSToolbarFlexibleSpaceItem,
+        "com.apple.finder.SRCH"
+    );
+    "TB Size Mode" = 1;
+}'
+
+    # Modify behaviour for "Save" modal window
+    defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
+    defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+    defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
+
+    # Enable spring loading for directories: https://www.youtube.com/watch?v=F9kdAxGe9SE
+    defaults write -g com.apple.springing.enabled -bool true
+    defaults write -g com.apple.springing.delay -float 0
+
+    # Automatically open a new Finder window when a volume is mounted
+    defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+    defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+    defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+    # Show item info near icons on the desktop and in other icon views
+    /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+    /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+    /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+    # Show item info to the right of the icons on the desktop
+    /usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:labelOnBottom false" ~/Library/Preferences/com.apple.finder.plist
+
+    # Enable snap-to-grid for icons on the desktop and in other icon views
     /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
     /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
     /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
-    message_info "Increase grid spacing for icons on the desktop and in other icon views"
+    # Increase grid spacing for icons on the desktop and in other icon views
     /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
     /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
     /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
 
-    message_info "Increase the size of icons on the desktop and in other icon views"
+    # Increase the size of icons on the desktop and in other icon views
     /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
     /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
     /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+
+    # Expand the following File Info panes:
+    # “General”, “Open with”, and “Sharing & Permissions”
+    defaults write com.apple.finder FXInfoPanesExpanded -dict \
+	           General -bool true \
+	           OpenWith -bool true \
+	           Privileges -bool true
+
+    # When performing a search, search the current folder by default
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+    # Show icons for hard drives, servers, and removable media on the desktop
+    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+    killall Finder
+
+    # Show Sidebar, but remove the Tags section.
+    defaults write com.apple.finder ShowSidebar -bool true
+    defaults write com.apple.finder ShowRecentTags -bool false
+
 }
 
 ###############################################################################
@@ -163,8 +294,61 @@ function osx::dock {
 
     message_info "Set Dock to auto-hide and remove the auto-hiding delay"
     defaults write com.apple.dock autohide -bool true
-    defaults write com.apple.dock autohide-delay -float 0
-    defaults write com.apple.dock autohide-time-modifier -float 0
+    defaults write com.apple.dock autohide-delay -float 0.05
+    defaults write com.apple.dock autohide-time-modifier -float 0.25
+
+    defaults write com.apple.dock magnification -bool true
+    defaults write com.apple.dock tilesize -int 54
+    defaults write com.apple.dock largesize -int 64
+    defaults write com.apple.dock minimize-to-application -bool true
+    defaults write com.apple.dock mineffect -string "scale"
+    defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+    defaults write com.apple.dock showhidden -bool true
+    defaults write com.apple.dock show-recents -bool false
+    defaults write com.apple.dock show-process-indicators -bool true
+
+}
+
+###############################################################################
+# 🎛 Mission Control
+###############################################################################
+function osx::missioncontrol {
+    message_info "Implement settings for mission control"
+    defaults write com.apple.dock expose-animation-duration -float 0.1
+    defaults write com.apple.dashboard mcx-disabled -bool true
+
+    # Don’t automatically rearrange Spaces based on most recent use
+    defaults write com.apple.dock mru-spaces -bool false
+
+    # Hot corners
+    # Possible values:
+    #  0: no-op
+    #  2: Mission Control
+    #  3: Show application windows
+    #  4: Desktop
+    #  5: Start screen saver
+    #  6: Disable screen saver
+    #  7: Dashboard
+    # 10: Put display to sleep
+    # 11: Launchpad
+    # 12: Notification Center
+
+    # Top left screen corner → Show application windows
+    defaults write com.apple.dock wvous-tl-corner -int 3
+    defaults write com.apple.dock wvous-tl-modifier -int 0
+
+    # Top right screen corner → Mission Control
+    defaults write com.apple.dock wvous-tr-corner -int 2
+    defaults write com.apple.dock wvous-tr-modifier -int 0
+
+    # Bottom left screen corner → Notification Center
+    defaults write com.apple.dock wvous-bl-corner -int 12
+    defaults write com.apple.dock wvous-bl-modifier -int 0
+    # Bottom right screen corner → Desktop
+    defaults write com.apple.dock wvous-br-corner -int 4
+    defaults write com.apple.dock wvous-br-modifier -int 0
+
+    killall Dock
 
 }
 
@@ -222,6 +406,43 @@ function osx::extras {
     git config --global alias.lastchange 'log -p --follow -n 1'
 }
 
+###############################################################################
+# Others
+###############################################################################
+function osx::others {
+
+    # Disable the sound effects on boot
+    sudo nvram SystemAudioVolume=" "
+
+    defaults write -g AppleShowScrollBars -string "Always"
+    defaults write -g NSWindowResizeTime -float 0.001
+
+    # Restart automatically if the computer freezes
+    sudo systemsetup -setrestartfreeze on
+
+    # Avoid creating .DS_Store files on network or USB volumes
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+    defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+    # Font rendering for non-retina displays. More info: https://github.com/Microsoft/vscode/issues/51132
+    defaults write -g CGFontRenderingFontSmoothingDisabled -bool false
+
+    # Set computer name (as done via System Preferences → Sharing)
+    sudo scutil --set ComputerName "${COMPUTER_NAME}"
+    sudo scutil --set HostName "${COMPUTER_NAME}"
+    sudo scutil --set LocalHostName "${COMPUTER_NAME}"
+    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${COMPUTER_NAME}"
+
+    # Disable new disks requests for Time Machine
+    defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+    # Auto-play videos when opened with QuickTime Player
+    defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen -bool true
+
+    # Prevent Photos from opening automatically when devices are plugged in
+    defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+}
+
 
 function osx::packages {
     message_info "Install packages for ${osx_package_name}"
@@ -241,12 +462,15 @@ function osx::post_install {
 
 function osx::sync {
     osx::ux
-    osx::general
+    osx::apple:
+    osx::keyboard
     osx::trackpad
     osx::screen
     osx::finder
     osx::dock
+    osx::missioncontrol
     osx::browser
     osx::messages
     osx::extras
+    osx::others
 }
